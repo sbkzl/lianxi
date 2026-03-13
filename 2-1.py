af870847d9611a478c9190fb477b1e48a635092e
sbkzl <xm2372674735@qq.com>
2026-03-14 01:12:21 +0800

    学习至17页

---
+++ 2-1.py
@@ -0,0 +1,2 @@
+message='Hello Python World!'
+print(message)#massege是变量当改变第一行代码输出也会改变
---
+++ 2-3-2.py
@@ -0,0 +1,5 @@
+f_name="abc"
+l_name="lovelace"
+full_name=f"{f_name} {l_name}"#f字符串 将花括号内变量替换为其值来设置字符串的形式
+print(f"Hello,{full_name.title()}!")#.title()表示对.之前的变量进行title方法既大写首字母
+#.upper()全大写.lower()全小写
